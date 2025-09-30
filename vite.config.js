a82ebaee0f7b29272b2ade6bd47333b2ae278c6b
Saiyeenn <bronhsaiyeen@gmail.com>
2025-10-01 00:11:42 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1' 
-    ? '/final_project/'  // For GitHub Pages
-    : '/',        // For Vercel and development
-})
+  base: "/project_final",    
+});
