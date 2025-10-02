3cd9dfe398ec9e57123eeda118a87206241c13e8
Saiyeenn <bronhsaiyeen24@gmail.com>
2025-10-02 21:02:32 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/final1_project",    
-});
+  base: '/project_final/'   // ត្រូវដាក់ឈ្មោះ repository
+})
